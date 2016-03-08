878d13279f5142e6d600889f2c42807d229e020c
Arturo Rivas <arturo.rivas@icloud.com>
2016-03-08 21:05:36 +0100

    A falta de revisar parece que está todo

--- Hamburguesas en el mundo/Datos.swift
+++ Hamburguesas en el mundo/Datos.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class ColeccionDePaises {
+    let paises = ["España", "Francia", "Portugal", "Alemania", "Inglaterra",
+        "Marruecos", "Holanda", "Italia", "Grecia", "Bélgica",
+        "Suiza", "Suecia", "Dinamarca", "República Checa", "Lituania",
+        "Andorra", "Letonia", "Turquía", "Rumanía", "Rusia"]
+    
+    func obtenPais() -> String {
+        return paises[Int(arc4random()) % paises.count]
+    }
+}
+
+class ColeccionDeHamburguesas {
+    let hamburguesas = ["De pollo", "Barbacoa", "Vegetal", "Tijuana", "Light",
+        "Vacuno", "Cerdo", "Andaluza", "Ibérica", "Gourmet",
+        "Doble", "Triple", "4 quesos", "Kobe", "Sabrosa",
+        "Picante", "Mini", "Especial", "Sorpresa", "De la casa"]
+    
+    func obtenHamburguesa() -> String {
+        return hamburguesas[Int(arc4random()) % hamburguesas.count]
+    }
+}
--- Hamburguesas en el mundo/ViewController.swift
+++ Hamburguesas en el mundo/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paisLabel: UILabel!
+    @IBOutlet weak var hamburguesaLabel: UILabel!
+    
+    let pais = ColeccionDePaises()
+    let hamburguesa = ColeccionDeHamburguesas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        paisLabel.text = "País: \(pais.obtenPais())"
+        hamburguesaLabel.text = "Hamburguesa: \(hamburguesa.obtenHamburguesa())"
+        
+        view.backgroundColor = colorAleatorio()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cambioPaisHamburguesa(sender: AnyObject) {
+        paisLabel.text = "País: \(pais.obtenPais())"
+        hamburguesaLabel.text = "Hamburguesa: \(hamburguesa.obtenHamburguesa())"
+        
+        view.backgroundColor = colorAleatorio()
+    }
+    
+    func colorAleatorio() -> UIColor {
+        let rojo = CGFloat(arc4random()) % 256
+        let verde = CGFloat(arc4random()) % 256
+        let azul = CGFloat(arc4random()) % 256
+        
+        return UIColor(red: rojo/255, green: verde/255, blue: rojo/255, alpha: azul/255)
+    }
 
 }
 
